b73e3b0d6b4f5d505bd65a541434866e88a75e30
Rudolf Ortner <rudolf.ortner.rottenbach@gmail.com>
2026-09-05 17:34:59 +0200

    Build Script checks for Latest Version

--- build.sh
+++ build.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
+# END SCRIPT ON FAILURE
+set -e
+
 # RETRIEVE VERSION TAG
 URL=https://www.sheepit-renderfarm.com/media/applet/client-latest.php
 LATEST_VERSION=$(curl --connect-timeout 20 --retry 4 --silent --show-error --head "$URL" |
     grep -Po '(?i)content-disposition:.*filename="?sheepit-client-\K\d+(?:\.\d+)*')
+    
+# CHECK LATEST VERSION
+if [[ -z "$LATEST_VERSION" ]]; then
+    echo "ERROR: Could not determine latest version."
+    exit 1
+fi
+
+# CHECK CURRENT IMAGE VERSION
+REMOTE_IMAGE=$(docker buildx imagetools inspect "ghcr.io/rudolfortner/sheepit-docker-client:$LATEST_VERSION" 2>/dev/null)
+
+if [[ -n "$REMOTE_IMAGE" ]]; then
+    echo "Version $LATEST_VERSION already exists in GHCR."
+    echo "Nothing to build."
+    exit 0
+fi
 
 # BUILD CONTAINER
-docker build .						\
-	-t sheepit-docker-client:latest			\
+echo "Building image for $LATEST_VERSION"
+docker build . \
+	-t sheepit-docker-client:latest	\
 	-t sheepit-docker-client:$LATEST_VERSION
 
-# TAGS FR GHCR
+# TAGS FOR GHCR
 docker tag sheepit-docker-client:latest ghcr.io/rudolfortner/sheepit-docker-client:latest
 docker tag sheepit-docker-client:$LATEST_VERSION ghcr.io/rudolfortner/sheepit-docker-client:$LATEST_VERSION
 
